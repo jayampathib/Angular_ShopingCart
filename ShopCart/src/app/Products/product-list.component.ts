4157d426ea65b49e742487d6c326f746f5623b70
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-06-18 21:59:05 +0800

    5.0 Model binding . add data array and diplayed them in table

--- ShopCart/src/app/Products/product-list.component.ts
+++ ShopCart/src/app/Products/product-list.component.ts
@@ -7,4 +7,26 @@ import { Component } from "@angular/core";
 export class ProductListComponent
 {
     pageTitle:string ='Product List';
+    products :any[]=[
+        {
+            "productId" : 1,
+            "productType" : "Dvd",
+            "category" : "Movie" ,
+            "name" :"Titanic" ,
+            "releaseDate" :"2000-01-31" ,
+            "price":3.25,
+            "starRating":8.2,
+            "coverImageUrl":""
+        },
+        {
+            "productId" : 2,
+            "productType" : "Dvd",
+            "category" : "Movie" ,
+            "name" :"Rambo" ,
+            "releaseDate" :"1988-08-31" ,
+            "price":1.25,
+            "starRating":6.4,
+            "coverImageUrl":""
+        }
+    ];
 }
