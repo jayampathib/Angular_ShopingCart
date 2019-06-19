d6fdfca8b9de30ba3ce727d9a968206fc2dfa358
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-06-20 00:40:56 +0800

    8.4.0 Pass Output : add Click event to child component

--- ShopCart/src/app/Shared/star.component.ts
+++ ShopCart/src/app/Shared/star.component.ts
@@ -13,4 +13,8 @@ export class StarComponent implements OnChanges
     ngOnChanges(): void {
         this.starWidth = this.rating * 75 / 5;
       }
+
+    onClick() :void{
+        alert('Hi');
+    }
 }
