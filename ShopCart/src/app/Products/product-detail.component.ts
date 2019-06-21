5a0979da4ce271476164d5adadf03a88caf354e7
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-06-22 00:05:34 +0800

    11.6.3 Readd query string value. olld method not working [let id = +this.route.snapshot.paramMap.get('id');]

--- ShopCart/src/app/Products/product-detail.component.ts
+++ ShopCart/src/app/Products/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 import { IProduct } from "./IProduct";
 
@@ -6,10 +7,20 @@ import { IProduct } from "./IProduct";
   //  selector : '<shp-product-detail>',
     templateUrl:'product-detail.component.html'
 })
-export class ProductDetailComponent 
+export class ProductDetailComponent implements OnInit
 {
     pageTitle: string = "Product Detail of Id ";
     product : IProduct;
  
+    constructor(private route : ActivatedRoute,
+        private router: Router) { 
 
+    }
+    ngOnInit(): void {
+         //let id = +this.route.snapshot.paramMap.get('id');
+         let id = +this.route.snapshot.params.id;
+         this.pageTitle += `: ${id}`;
+    }
+
+    
 }
