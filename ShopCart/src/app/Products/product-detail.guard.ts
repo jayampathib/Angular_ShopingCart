f716ff001e0dc95fbb00fb4bec3d1926df545e6a
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-06-22 01:09:51 +0800

    11.8.2 if routeguard validation fail , return to previous page

--- ShopCart/src/app/Products/product-detail.guard.ts
+++ ShopCart/src/app/Products/product-detail.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Route } from '@angular/router';
 import { Observable } from 'rxjs';
 //import { Promise } from 'q';
 
@@ -7,13 +7,17 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductDetailGuard implements CanActivate {
+ 
+  constructor(router : Route) {
+    
+  }
   
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) : Observable<boolean> | Promise<boolean> | boolean {
     
     let id = +route.url[1].path;
     if (isNaN(id) || id < 1) {
       alert('Invalid product Id');
-      
+      this.router.navigate(['/products']);
       return false;
     }
     return true;
